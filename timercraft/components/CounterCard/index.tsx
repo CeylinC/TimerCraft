4f3f3d12ecc5a9475ce6909433a3c164aaba81a4
Ceylin <caltepeceylin@gmail.com>
2024-04-13 16:32:06 +0300

    feat: create header component

--- timercraft/components/CounterCard/index.tsx
+++ timercraft/components/CounterCard/index.tsx
@@ -1,7 +1,11 @@
 import Counter from "../Counter";
 import { IProp } from "./_type";
 
-export default function CounterCard({ count, cardTitle, className }: IProp) {
+export default function CounterCard({
+  count,
+  cardTitle,
+  className,
+}: IProp) {
   const numberArr = count.toString().split("");
   if (numberArr.length < 2) {
     numberArr.unshift("0");
@@ -10,10 +14,10 @@ export default function CounterCard({ count, cardTitle, className }: IProp) {
     <div className={className}>
       <div className="flex flex-row flex-nowrap">
         {numberArr.map((item, i) => {
-          return <Counter number={item} key={i} />;
+          return <Counter number={item} key={i}/>;
         })}
       </div>
-      <p className="text-[10px] text-center">{cardTitle}</p>
+      <p className="text-[10px] text-center text-text">{cardTitle}</p>
     </div>
   );
 }
---
+++ timercraft/model/TimerForm.ts
@@ -0,0 +1,20 @@
+export interface TimerForm {
+  timerStyle: string;
+  closeButton: boolean;
+  timerTitle: string;
+  setTime: string;
+  remainingTimePeriod: number;
+  positioning: string;
+  displayCount: {
+    days: boolean;
+    hours: boolean;
+    minutes: boolean;
+    seconds: boolean;
+  };
+  daysLabel: string;
+  hoursLabel: string;
+  minutesLabel: string;
+  secondsLabel: string;
+  buttonText: string;
+  buttonLink: string;
+}
